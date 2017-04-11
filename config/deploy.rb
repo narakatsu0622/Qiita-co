ea7a871f7967ca2a97f1270188b6731f30f155be
narakatsu0622 <narakatsu0622+github@gmail.com>
2017-04-11 17:39:25 +0900

    自動デプロイ環境設定

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "3.8.0"
 
 set :application, 'Qiita-co'
-set :repo_url, 'git@github.com:Qiita-co.git'
+set :repo_url, 'git@github.com:narakatsu0622/Qiita-co.git'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
