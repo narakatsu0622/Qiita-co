ad7b1c67db2c5af3454d2771b8c7cb1dc9f5b485
narakatsu0622 <narakatsu0622+github@gmail.com>
2017-04-11 17:05:54 +0900

    自動デプロイ環境設定

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.3.1'
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['C:\Users\dadad\vagrant\.ssh']
+                  keys: ['C:\Users\dadad\vagrant\.ssh\keypair.pem']
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
